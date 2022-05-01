ea80cd378be15f45d8915a08a2c314950f4610de
sbx_user1051 <sbx_user1051@169.254.95.197>
2022-05-01 13:51:21 +0000

    Sun May 01 13:51:21 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,40 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    
+    price_table = {
+                    "A" : 50,
+                    "B" : 30,
+                    "C" : 20,
+                    "D" : 15,
+                  }
+    
+
+
+    sku_list = [unit for unit in skus]
+
+    for unit in sku_list:
+        if unit not in price_table:
+            return -1
+    
+    sku_count = {}
+
+    for unit in sku_list:
+        sku_count[unit] = sku_count.get(unit, 0) + 1
+    
+    total_checkout_value = 0
+
+    for unit in price_table.keys():
+
+        if unit == "A":
+            discounted, non_discounted = divmod(sku_count[unit], 3)
+            total_checkout_value += discounted * 130
+            total_checkout_value += non_discounted * price_table[unit]
+        elif unit == "B":
+            discounted, non_discounted = divmod(sku_count[unit], 2)
+            total_checkout_value += discounted * 45
+            total_checkout_value += non_discounted * price_table[unit]
+        else:
+            total_checkout_value += price_table[unit] * sku_count[unit]
+    
+    return total_checkout_value
---
+++ test/solution_tests/CHK/test_chk.py
@@ -0,0 +1,5 @@
+from solutions.CHK import checkout_solution
+
+class TestHello():
+    def test_checkout(self):
+        assert checkout_solution.hello("AAA") == 130
